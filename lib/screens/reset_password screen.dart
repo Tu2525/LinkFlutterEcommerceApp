dc2560a0c6514c1c4fa5a6d2ccf1907b41e3b76d
Abdllah_Atef <abdllahatef7@gmail.com>
2025-07-11 19:57:28 +0300

    finish reset password screen

--- lib/screens/reset_password screen.dart
+++ lib/screens/reset_password screen.dart
@@ -5,14 +5,71 @@ class ResetPassword extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-         
-        ],
-      )
+    final size = MediaQuery.of(context).size;
+    final screenWidth = size.width;
+
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            width: screenWidth * 0.85,
+            padding: const EdgeInsets.symmetric(vertical: 24),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'images/2a7988bf70f4548361b00378b3d52898cf023ae2.png',
+                  height: screenWidth * 0.25,
+                  width: screenWidth * 0.25,
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'We Sent you an Email to reset',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Circular',
+                    fontWeight: FontWeight.w500,
+                    fontSize: 23,
+                  ),
+                ),
+                const Text(
+                  'your password.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Circular',
+                    fontWeight: FontWeight.w500,
+                    fontSize: 23,
+                  ),
+                ),
+                const SizedBox(height: 24),
+                SizedBox(
+                  width: screenWidth * 0.5,
+                  height: 52,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xff8E6CEF),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      elevation: 0,
+                    ),
+                    child: const Text(
+                      'Return to Login',
+                      style: TextStyle(
+                        fontFamily: 'Circular',
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
