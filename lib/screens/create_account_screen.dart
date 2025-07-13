ac2091f6879ec0b1574c7343c0cc8297c0eb0ab7
unknown <tarek.darwish@aispeakapp.com>
2025-07-13 21:44:37 +0300

    Added sign up screen, and updated navigation for sign_in_screen

---
+++ lib/screens/create_account_screen.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:link_flutter_ecommerce_app/screens/reset_password%20screen.dart';
+import 'package:link_flutter_ecommerce_app/widgets/continue_button.dart';
+import 'package:link_flutter_ecommerce_app/widgets/custom_back_icon.dart';
+import 'package:link_flutter_ecommerce_app/widgets/custom_text_field.dart';
+
+class CreateAccountScreen extends StatelessWidget {
+  CreateAccountScreen({super.key});
+
+  final emailController = TextEditingController();
+  final firstnameController = TextEditingController();
+  final lastnameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        leading: const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: CustomIcon(),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+
+            children: [
+              const SizedBox(height: 20),
+              const Text(
+                "Create Account",
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(height: 32),
+              CustomTextField(
+                emailController: firstnameController,
+                isPassword: false,
+                hint: "Firstname",
+              ),
+              const SizedBox(height: 16),
+              CustomTextField(
+                emailController: lastnameController,
+                isPassword: false,
+                hint: "Lastname",
+              ),
+              const SizedBox(height: 16),
+              CustomTextField(
+                emailController: emailController,
+                isPassword: false,
+                hint: "Email Address",
+              ),
+              const SizedBox(height: 16),
+              CustomTextField(
+                emailController: passwordController,
+                isPassword: true,
+                hint: "Password",
+              ),
+              const SizedBox(height: 40),
+              ContinueButton(onPressed: (){}),
+              const SizedBox(height: 40,),
+              Row(
+                children: [
+                  Text(
+                    'Forgot Password?',
+                    style: Theme.of(
+                      context,
+                    ).textTheme.bodySmall?.copyWith(fontFamily: 'Circular'),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const ResetPassword(),
+                    ),
+                  );
+                },
+                    child: Text(
+                      ' Reset',
+                      style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Circular',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
