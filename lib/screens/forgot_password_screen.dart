6340a1b53ab49f3699913428aaab5760a554ffe4
Abdllah_Atef <abdllahatef7@gmail.com>
2025-07-14 13:56:34 +0300

    fix mediaQuery from .of to .sizeOf

--- lib/screens/forgot_password_screen.dart
+++ lib/screens/forgot_password_screen.dart
@@ -11,8 +11,8 @@ class ForgotPassword extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final screenHeight = MediaQuery.of(context).size.height;
-    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.sizeOf(context).height;
+    final screenWidth = MediaQuery.sizeOf(context).width;
 
     return Scaffold(
       body: SafeArea(
