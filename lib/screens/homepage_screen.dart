80cd511feb0b6098952d87e34ce6a93f6b517223
Tu2525 <tareksheren@gmail.com>
2025-07-21 15:51:09 +0300

    .

--- lib/screens/homepage_screen.dart
+++ lib/screens/homepage_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax_plus/iconsax_plus.dart';
 import 'package:link_flutter_ecommerce_app/constants/app_colors.dart';
+import 'package:link_flutter_ecommerce_app/widgets/categories.dart';
+import 'package:link_flutter_ecommerce_app/widgets/categories_section.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,68 +19,71 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 24.0),
+        padding: const EdgeInsets.symmetric(vertical: 24.0),
         child: Column(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                // Avatar
-                const CircleAvatar(
-                  radius: 20,
-                  backgroundImage: NetworkImage(
-                    'https://picsum.photos/536/354',
-                  ),
-                ),
-
-                PopupMenuButton<String>(
-                  onSelected: (String result) {
-                    setState(() {
-                      _selectedCategory = result;
-                    });
-                  },
-                  itemBuilder:
-                      (BuildContext context) =>
-                          _categories.map((String category) {
-                            return PopupMenuItem<String>(
-                              value: category,
-                              child: Text(category),
-                            );
-                          }).toList(),
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 20,
-                      vertical: 10,
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.grey[200],
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    child: Row(
-                      children: [
-                        Text(
-                          _selectedCategory,
-                          style: const TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(width: 4),
-                        const Icon(IconsaxPlusBroken.arrow_down_2, size: 20),
-                      ],
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // Avatar
+                  const CircleAvatar(
+                    radius: 20,
+                    backgroundImage: NetworkImage(
+                      'https://picsum.photos/536/354',
                     ),
                   ),
-                ),
-                Container(
-                  width: 40,
-                  height: 40,
-                  decoration: const BoxDecoration(
-                    color: AppColors.primary,
-                    shape: BoxShape.circle,
+              
+                  PopupMenuButton<String>(
+                    onSelected: (String result) {
+                      setState(() {
+                        _selectedCategory = result;
+                      });
+                    },
+                    itemBuilder:
+                        (BuildContext context) =>
+                            _categories.map((String category) {
+                              return PopupMenuItem<String>(
+                                value: category,
+                                child: Text(category),
+                              );
+                            }).toList(),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 10,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Row(
+                        children: [
+                          Text(
+                            _selectedCategory,
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(width: 4),
+                          const Icon(IconsaxPlusBroken.arrow_down_2, size: 20),
+                        ],
+                      ),
+                    ),
                   ),
-                  child: const Icon(
-                    IconsaxPlusBroken.bag_2,
-                    color: Colors.white,
+                  Container(
+                    width: 40,
+                    height: 40,
+                    decoration: const BoxDecoration(
+                      color: AppColors.primary,
+                      shape: BoxShape.circle,
+                    ),
+                    child: const Icon(
+                      IconsaxPlusBroken.bag_2,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
 
             const SizedBox(height: 20),
@@ -87,7 +92,7 @@ class _HomePageState extends State<HomePage> {
               padding: const EdgeInsets.symmetric(horizontal: 20),
               decoration: BoxDecoration(
                 color: AppColors.grey,
-                borderRadius: BorderRadius.circular(100),
+                borderRadius: BorderRadius.circular(20),
               ),
               child: const TextField(
                 decoration: InputDecoration(
@@ -97,12 +102,7 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
             ),
-
-            const SizedBox(height: 20),
-
-            const Expanded(
-              child: Center(child: Text('7oto el 7agat el taniya')),
-            ),
+            CategoriesSection(),
           ],
         ),
       ),
--- lib/widgets/categories_section.dart
+++ lib/widgets/categories_section.dart
@@ -5,59 +5,87 @@ import 'package:link_flutter_ecommerce_app/widgets/categories.dart';
 class CategoriesSection extends StatelessWidget {
   CategoriesSection({super.key});
 
-
-  final List<CategoryModel> allCategories= [
-    CategoryModel(categoryName:'Hoodies' , imgPath: 'images/Ellipse 1.png', ontap: (){}),
-    CategoryModel(categoryName:'Shorts' , imgPath:  'images/Ellipse 2.png', ontap: (){}),
-    CategoryModel(categoryName:'Shoes' , imgPath: 'images/Ellipse 3.png', ontap: (){}),
-    CategoryModel(categoryName:'Bag', imgPath: 'images/Ellipse 4.png', ontap: (){}),
-    CategoryModel(categoryName:'Accessories', imgPath: 'images/Ellipse 3 (1).png', ontap: (){}),
+  final List<CategoryModel> allCategories = [
+    CategoryModel(
+      categoryName: 'Hoodies',
+      imgPath: 'images/Ellipse 1.png',
+      ontap: () {},
+    ),
+    CategoryModel(
+      categoryName: 'Shorts',
+      imgPath: 'images/Ellipse 2.png',
+      ontap: () {},
+    ),
+    CategoryModel(
+      categoryName: 'Shoes',
+      imgPath: 'images/Ellipse 3.png',
+      ontap: () {},
+    ),
+    CategoryModel(
+      categoryName: 'Bag',
+      imgPath: 'images/Ellipse 4.png',
+      ontap: () {},
+    ),
+    CategoryModel(
+      categoryName: 'Accessories',
+      imgPath: 'images/Ellipse 3 (1).png',
+      ontap: () {},
+    ),
   ];
-
 
   @override
   Widget build(BuildContext context) {
-
     final isDarkMode = Theme.of(context).brightness == Brightness.dark;
     final screenHeight = MediaQuery.of(context).size.height;
-    return  Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          children: [
-            SizedBox(
-            height: screenHeight *0.09,
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              SizedBox(height: screenHeight * 0.09),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Text(
+                      "Categories",
+                      style: TextStyle(
+                        color: isDarkMode ? Colors.white : Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Spacer(flex: 1),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Text(
+                        "See All",
+                        style: TextStyle(
+                          color: isDarkMode ? Colors.white : Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Row(children: getAllCategories()),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(children: [
-              Text("Categories",
-              style: TextStyle(
-                color: isDarkMode ? Colors.white : Colors.black,
-                fontWeight: FontWeight.bold
-              ),),
-              const Spacer(flex: 1,),
-              GestureDetector(
-                onTap: (){  },
-                child: Text("See All",style: TextStyle(
-                  color: isDarkMode ? Colors.white : Colors.black,
-                ),)),
-            ],),
-          ),
-          Row(
-            children: getAllCategories(),
-          )],
         ),
-      ),
+      ],
     );
   }
 
-
-  List<Categories> getAllCategories(){
-    return allCategories.map((e) => Categories(
-    imgPath: e.imgPath,
-    title: e.categoryName,
-    ontap: e.ontap,
-    )).toList();
+  List<Categories> getAllCategories() {
+    return allCategories
+        .map(
+          (e) => Categories(
+            imgPath: e.imgPath,
+            title: e.categoryName,
+            ontap: e.ontap,
+          ),
+        )
+        .toList();
   }
 }
