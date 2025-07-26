71605970d9f1c31d8c1913b328a74ce8275c917f
Abdllah_Atef <abdllahatef7@gmail.com>
2025-07-27 01:16:34 +0300

    fix custom text field &add validator to forgot pass screen

--- lib/screens/forgot_password_screen.dart
+++ lib/screens/forgot_password_screen.dart
@@ -4,10 +4,22 @@ import 'package:link_flutter_ecommerce_app/widgets/continue_button.dart';
 import 'package:link_flutter_ecommerce_app/widgets/custom_back_icon.dart';
 import 'package:link_flutter_ecommerce_app/widgets/custom_text_field.dart';
 
-class ForgotPassword extends StatelessWidget {
-  ForgotPassword({super.key});
+class ForgotPassword extends StatefulWidget {
+  const ForgotPassword({super.key});
 
+  @override
+  State<ForgotPassword> createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPassword> {
   final TextEditingController emailController = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,38 +36,51 @@ class ForgotPassword extends StatelessWidget {
               horizontal: screenWidth * 0.06,
               vertical: screenHeight * 0.05,
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const CustomIcon(),
-                SizedBox(height: screenHeight * 0.03),
-                const Text(
-                  'Forgot Password?',
-                  style: TextStyle(
-                    fontFamily: 'Circular',
-                    fontWeight: FontWeight.w700,
-                    fontSize: 32,
+            child: Form(
+              key: formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const CustomIcon(),
+                  SizedBox(height: screenHeight * 0.03),
+                  const Text(
+                    'Forgot Password?',
+                    style: TextStyle(
+                      fontFamily: 'Circular',
+                      fontWeight: FontWeight.w700,
+                      fontSize: 32,
+                    ),
                   ),
-                ),
-                SizedBox(height: screenHeight * 0.04),
-                CustomTextField(
-                  emailController: emailController,
-                  isPassword: false,
-                  hint: 'Enter Email address',
-                  isdark: isDarkMode,
-                ),
-                SizedBox(height: screenHeight * 0.03),
-                ContinueButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const ResetPassword(),
-                      ),
-                    );
-                  },
-                ),
-              ],
+                  SizedBox(height: screenHeight * 0.04),
+                  CustomTextField(
+                    emailController: emailController,
+                    isPassword: false,
+                    hint: 'Enter Email address',
+                    isdark: isDarkMode,
+
+                    validator:
+                        (value) =>
+                            (value == null || value.trim().isEmpty)
+                                ? 'Please enter your email'
+                                : !value.contains('@') || !value.contains('.')
+                                ? 'Please enter a valid email address'
+                                : null,
+                  ),
+                  SizedBox(height: screenHeight * 0.03),
+                  ContinueButton(
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const ResetPassword(),
+                          ),
+                        );
+                      }
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ),
