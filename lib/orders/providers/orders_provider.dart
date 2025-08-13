911d7ec6cfc9098f90e19ed79fed435242307e90
Tu2525 <tareksheren@gmail.com>
2025-08-13 10:57:03 +0300

    even more fixes...

--- lib/orders/providers/orders_provider.dart
+++ lib/orders/providers/orders_provider.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:link_flutter_ecommerce_app/orders/models/order_model.dart';
 import 'package:link_flutter_ecommerce_app/orders/services/order_service.dart';
@@ -13,7 +14,17 @@ final orderProvider = FutureProvider<List<OrderModel>>((ref) {
   return api.getOrderDetails();
 });
 
+final authStateChangesProvider = StreamProvider<User?>((ref) {
+  return FirebaseAuth.instance.authStateChanges();
+});
+
 final ordersStreamProvider = StreamProvider<List<OrderModel>>((ref) {
-  final api = ref.watch(orderDetailsProvider);
-  return api.getOrdersStream();
+  final authState = ref.watch(authStateChangesProvider);
+
+  if (authState.asData?.value != null) {
+    final api = ref.watch(orderDetailsProvider);
+    return api.getOrdersStream();
+  }
+
+  return Stream.value([]);
 });
